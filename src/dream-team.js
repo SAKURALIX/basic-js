4ed53ca92018d4f830163c91174ffac49b0557bd
SAKURALIX <sakura.xlix@gmail.com>
2023-04-15 10:29:32 +0300

    feat: add dream team solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -16,14 +16,31 @@ const { NotImplementedError } = require('../extensions/index.js');
 function createDreamTeam(members) {
   // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+
   let dreamTeamName = '';
-  let sortedMembers = members.sort();
-  for (let i = 0; i < sortedMembers.length; i++) {
-    dreamTeamName = dreamTeamName + sortedMembers[i][0];
+  if(!Array.isArray(members)) {return dreamTeamName = false}
+  else {
+      
+      let arr2 = [];
+      for (let i = 0; i < members.length; i++) {
+        if(typeof members[i] === 'string') {
+          let newString = members[i].replace(/ /g,'').toUpperCase();
+          arr2.push(newString)
+        }
+      }
+  
+      let sortedMembers = arr2.sort();
+    
+      for (let i = 0; i < sortedMembers.length; i++) {
+          dreamTeamName = dreamTeamName + sortedMembers[i][0];
+      }
+
   }
+
+
   return dreamTeamName
 }
-console.log(createDreamTeam(['Peter', 'Adam', 'Gary', 'Bobby', 'Philip', 'Andrea', 'Evelyn', 'Evelyn', 'Kimberly', 'Denise']))
+console.log(createDreamTeam(['    Peter', 'Adam', 'Gary', 'Bobby', 'Philip', 'Andrea', 'Evelyn', 'Evelyn', 'Kimberly', 'Denise']))
 // 'AABDEEGKPP'
 
 module.exports = {
